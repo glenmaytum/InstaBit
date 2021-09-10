28076e5e83ce811d2bc5def12f3c2ce099a17007
Glen Maytum <glenmaytum@gmail.com>
2021-09-11 00:14:47 +0100

    changed instances to correct name

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Decentragram = artifacts.require("Decentragram");
+const Instablock = artifacts.require("Instablock");
 
 module.exports = function(deployer) {
-  // Code goes here...
+	// Code goes here...
 };
